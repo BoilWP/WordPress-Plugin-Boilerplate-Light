0e951fb847caff535c00c43167fb65fa27f45a54
Sebastien <sebastien@sebs-studio.com>
2015-03-17 13:51:09 +0100

    Updated the language files

--- wordpress-plugin-boilerplate-light/uninstall.php
+++ wordpress-plugin-boilerplate-light/uninstall.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Runs on Uninstall of [Plugin Name]
+ * Runs on Uninstall of Plugin Name
  *
  * This file will remove any options and tables you
  * have created for this plugin.
  *
- * @since     1.0.0
- * @author 		Your Name / Your Company Name
- * @category 	Core
- * @package 	Plugin Name
- * @license   GPL-2.0+
+ * @since    1.0.0
+ * @author   Your Name / Your Company Name
+ * @category Core
+ * @package  Plugin Name
+ * @license  GPL-2.0+
  */
 if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit();
 
@@ -24,16 +24,6 @@ if ( ! is_multisite() ) {
 
 		// Delete options
 		$wpdb->query("DELETE FROM $wpdb->options WHERE option_name LIKE 'plugin_name_%';");
-
-		// Roles + caps
-		$installer = include( 'includes/admin/class-plugin-name-install.php' );
-		$installer->remove_roles();
-
-		// Pages
-		$get_pages = $installer->plugin_name_pages();
-		foreach( $get_pages as $key => $page ) {
-			wp_trash_post( get_option( 'plugin_name_' . $key . '_page_id' ) );
-		}
 
 		/*
 		 * @todo Place your own uninstall code here.
